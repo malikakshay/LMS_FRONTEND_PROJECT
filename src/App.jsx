2a5e99fc4d7d068826f5153f4960d91b0fe553c2
AKSHAY MALIK <malikakshaya4200@gmail.com>
2024-05-05 16:22:26 +0530

    Started adding basic redux setup with toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 
+import { Routes } from 'react-router-dom'
+
+
 function App() {
 
   return (
    <>
-   LMS
+     <Routes>
+       {/* {<Route path='/' element= {<Home />}></Route>} */}
+     </Routes>
     </>
   )
 }
